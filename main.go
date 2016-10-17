35696ed37a64e82dafe01a8c045b236daf09c233
edwin <edwin@homerlogistics.com>
2016-10-17 12:23:44 -0400

    Add os get port

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "net/http"
     "github.com/gorilla/mux"
     "cumberbatch"
+    "os"
 )
 
 func Generate(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +17,7 @@ func main() {
   router := mux.NewRouter().StrictSlash(true)
   router.HandleFunc("/", Generate)
 
-  port := ":8000"
+  port := os.Getenv("PORT")
   fmt.Println("Starting server on port", port)
-  log.Fatal(http.ListenAndServe(port, router))
+  log.Fatal(http.ListenAndServe(":" + port, router))
 }
